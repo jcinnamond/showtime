605a225bb101865ecad63f02f9e1b016c2202260
John Cinnamond <john@cinnamond.me.uk>
2022-04-03 17:22:45 +0100

    Use delete from Data.List
    I had defined my own crappy version of this.

--- app/Main.hs
+++ app/Main.hs
@@ -3,27 +3,28 @@
 
 module Main where
 
-import Data.List (sortOn, unfoldr)
+import Data.List (delete, sortOn, unfoldr)
 import Data.Text (Text, intercalate)
 import qualified Data.Text.IO as TIO
 import Fmt
 import System.Random
 
 type Person = Text
+
 data Cast = Cast
-  { host :: Person
-  , participants :: [Person]
+  { host :: Person,
+    participants :: [Person]
   }
 
 ppCast :: Cast -> Text
-ppCast Cast{host, participants} =
+ppCast Cast {host, participants} =
   fmt $ "Host: " +| host |+ "\nParticipants: " +| intercalate ", " participants |+ "\n"
 
 takeRandom :: RandomGen g => g -> Int -> [a] -> [a]
 takeRandom g n xs = take n $ fst <$> sortOn snd (zip xs randoms)
- where
-  randoms :: [Word]
-  randoms = unfoldr (Just . uniform) g
+  where
+    randoms :: [Word]
+    randoms = unfoldr (Just . uniform) g
 
 hosts :: [Person]
 hosts = ["Betty", "Freddy"]
@@ -31,13 +32,9 @@ hosts = ["Betty", "Freddy"]
 people :: [Person]
 people = hosts ++ ["Bippy", "Boppy", "Hoppy", "Hetty"]
 
-without :: Eq a => a -> [a] -> [a]
-without i [] = []
-without i (x : xs) = if i == x then without i xs else x : without i xs
-
 main :: IO ()
 main = do
   gen <- getStdGen
   let host = head $ takeRandom gen 1 hosts
-  let ps = takeRandom gen 3 (without host people)
+  let ps = takeRandom gen 3 (delete host people)
   TIO.putStr $ ppCast $ Cast host ps
