d9d57d14f9533f58f3bb9fd2f7fc95097032257a
John Cinnamond <john@cinnamond.me.uk>
2023-08-31 20:38:34 +0100

    Add topics to episodes

--- src/Episodes/Commands.hs
+++ src/Episodes/Commands.hs
@@ -1,11 +1,10 @@
-module Episodes.Commands
-  ( run,
-  )
+module Episodes.Commands (
+  run,
+)
 where
 
 import Application (App, AppEnv (participantCount))
 import Cast.Cast (Cast (..), Host (..), Participant (..))
-import qualified Cast.Cast as Cast
 import qualified Cast.Storage as CastStorage
 import Control.Monad.IO.Class (liftIO)
 import Control.Monad.Trans.Reader (asks)
@@ -19,18 +18,25 @@ import qualified Episodes.Episode as Episode
 import qualified Episodes.Storage as Storage
 import qualified Storage as S
 import System.Random (RandomGen, getStdGen, uniform)
+import Text.Read (readMaybe)
+import qualified Topics.Storage as TopicStorage
+import Topics.Topic (Topic (..))
 
 run :: (S.Storage s) => [String] -> App s ()
 run ["new"] = runNew
 run ["replace", oldName] = runReplace $ T.pack oldName
-run ["show"] = runListEpisodes
+run ["show"] = runShowEpisode
+run ["topic", "select", topic] = runSelectTopic topic
+run ["topic", "clear"] = runClearTopic
 run x = error $ "unexpected command: " <> show x
 
 runNew :: (S.Storage s) => App s ()
 runNew = do
   cast <- pickCast
-  Storage.new $ Episode cast
-  liftIO $ TIO.putStrLn $ Cast.pretty cast
+  topics <- suggestTopics 3
+  let episode = Episode cast topics Nothing
+  Storage.new episode
+  liftIO $ TIO.putStrLn $ Episode.pretty episode
 
 pickCast :: (S.Storage s) => App s Cast
 pickCast = do
@@ -43,14 +49,20 @@ pickCast = do
   let ps = takeRandom gen count allParticipants
   pure $ Cast host ps
 
+suggestTopics :: (S.Storage s) => Int -> App s [Topic]
+suggestTopics n = do
+  topics <- TopicStorage.list
+  gen <- getStdGen
+  pure $ takeRandom gen n topics
+
 takeRandom :: (RandomGen g) => g -> Int -> [a] -> [a]
 takeRandom g n xs = take n $ fst <$> sortOn snd (zip xs rs)
-  where
-    rs :: [Word]
-    rs = unfoldr (Just . uniform) g
+ where
+  rs :: [Word]
+  rs = unfoldr (Just . uniform) g
 
-runListEpisodes :: (S.Storage s) => App s ()
-runListEpisodes = do
+runShowEpisode :: (S.Storage s) => App s ()
+runShowEpisode = do
   episode <- Storage.load
   let p = maybe "no episode planned" Episode.pretty episode
   liftIO $ TIO.putStrLn p
@@ -65,16 +77,16 @@ runReplace :: (S.Storage s) => Text -> App s ()
 runReplace name = do
   episode <- Storage.load
   case findCastMember episode of
-    (ReplaceHost e) -> replaceHost name e >> runListEpisodes
-    (ReplaceParticipant e) -> replaceParticipant name e >> runListEpisodes
+    (ReplaceHost e) -> replaceHost name e >> runShowEpisode
+    (ReplaceParticipant e) -> replaceParticipant name e >> runShowEpisode
     NoEpisode -> liftIO $ TIO.putStrLn "no episode is planned"
     NotFound -> liftIO $ TIO.putStrLn $ name <> " is not a cast member for the episode"
-  where
-    findCastMember Nothing = NoEpisode
-    findCastMember (Just e)
-      | name == (getHost . castHost . getCast) e = ReplaceHost e
-      | name `elem` (map getParticipant . castParticipants . getCast) e = ReplaceParticipant e
-      | otherwise = NotFound
+ where
+  findCastMember Nothing = NoEpisode
+  findCastMember (Just e)
+    | name == (getHost . castHost . getCast) e = ReplaceHost e
+    | name `elem` (map getParticipant . castParticipants . getCast) e = ReplaceParticipant e
+    | otherwise = NotFound
 
 replaceHost :: (S.Storage s) => Text -> Episode -> App s ()
 replaceHost _ episode = do
@@ -82,7 +94,7 @@ replaceHost _ episode = do
   hosts <- CastStorage.loadHosts
   let potentialHosts = removeCast (episode.getCast) (getHost <$> hosts)
   let newHost = head $ takeRandom gen 1 potentialHosts
-  let newEpisode = Episode $ Cast (Host newHost) (castParticipants . getCast $ episode)
+  let newEpisode = Episode (Cast (Host newHost) (castParticipants . getCast $ episode)) (episode.suggestedTopics) (episode.topic)
   Storage.new newEpisode
 
 replaceParticipant :: (S.Storage s) => Text -> Episode -> App s ()
@@ -93,12 +105,33 @@ replaceParticipant name episode = do
   let potentialParticipants = removeCast (episode.getCast) ((getHost <$> hosts) <> (getParticipant <$> participants))
   let newParticipant = head $ takeRandom gen 1 potentialParticipants
   let newParticipants = Participant newParticipant : L.delete (Participant name) (castParticipants . getCast $ episode)
-  let newEpisode = Episode $ Cast (castHost . getCast $ episode) newParticipants
+  let newEpisode = Episode (Cast (castHost . getCast $ episode) newParticipants) (episode.suggestedTopics) (episode.topic)
   Storage.new newEpisode
 
 removeCast :: Cast -> [Text] -> [Text]
 removeCast cast names = names \\ castMembers
-  where
-    participants = getParticipant <$> cast.castParticipants
-    host = getHost cast.castHost
-    castMembers = host : participants
+ where
+  participants = getParticipant <$> cast.castParticipants
+  host = getHost cast.castHost
+  castMembers = host : participants
+
+runSelectTopic :: (S.Storage s) => String -> App s ()
+runSelectTopic t = do
+  e <- Storage.load
+  case e of
+    Nothing -> liftIO $ TIO.putStrLn "no episode is planned"
+    Just episode -> do
+      let idx = readMaybe t :: Maybe Int
+      let topic = case idx of
+            Just i -> episode.suggestedTopics !! (i - 1)
+            Nothing -> Topic (T.pack t)
+          newEpisode = episode{topic = Just topic}
+      Storage.new newEpisode
+
+runClearTopic :: (S.Storage s) => App s ()
+runClearTopic = do
+  e <- Storage.load
+  case e of
+    Nothing -> liftIO $ TIO.putStrLn "no episode is planned"
+    Just episode -> do
+      Storage.new episode{topic = Nothing}
--- src/Episodes/Episode.hs
+++ src/Episodes/Episode.hs
@@ -1,20 +1,24 @@
-{-# LANGUAGE InstanceSigs #-}
-
 module Episodes.Episode (
   Episode (..),
   pretty,
 )
 where
 
-import Cast.Cast (Cast (..))
+import Cast.Cast (Cast (..), Host (..), Participant (..))
 import qualified Cast.Cast as Cast
 import qualified Data.List as L
 import Data.Maybe (mapMaybe)
 import Data.Text (Text)
 import qualified Data.Text as T
 import Storage (Storeable (..))
+import Topics.Topic (Topic (..))
+import qualified Topics.Topic as Topic
 
-newtype Episode = Episode {getCast :: Cast}
+data Episode = Episode
+  { getCast :: Cast
+  , suggestedTopics :: [Topic]
+  , topic :: Maybe Topic
+  }
   deriving stock (Eq, Show)
 
 instance Storeable Episode where
@@ -22,19 +26,31 @@ instance Storeable Episode where
   unmarshall = unmarshallEpisode
 
 marshallEpisode :: Episode -> Text
-marshallEpisode (Episode cast) =
+marshallEpisode (Episode cast topics topic) =
   L.foldl (<>) "" $
     L.intersperse "\0" $
-      [marshall cast.castHost] <> (marshall <$> cast.castParticipants)
+      [marshallHost] <> marshallParticipant <> marshallSuggestedTopics <> [marshallTopic]
+ where
+  marshallHost = "host " <> cast.castHost.getHost
+  marshallParticipant = map ("participant " <>) (Cast.getParticipant <$> cast.castParticipants)
+  marshallSuggestedTopics = map ("suggested topic " <>) (Topic.getTopic <$> topics)
+  marshallTopic = maybe "" (("topic " <>) . getTopic) topic
 
 unmarshallEpisode :: Text -> Maybe Episode
 unmarshallEpisode t = do
   let ts = T.splitOn "\0" t
-  let host = firstJust unmarshall ts
-  let participants = mapMaybe unmarshall ts
+  let host = firstJust unmarshallHost ts
+  let participants = mapMaybe unmarshallParticipant ts
+  let topics = mapMaybe unmarshallSuggestedTopic ts
+  let topic = firstJust unmarshallTopic ts
   case host of
-    Just h -> Just $ Episode $ Cast h participants
+    Just h -> Just $ Episode (Cast h participants) topics topic
     Nothing -> Nothing
+ where
+  unmarshallHost x = Host <$> T.stripPrefix "host " x
+  unmarshallParticipant x = Participant <$> T.stripPrefix "participant " x
+  unmarshallSuggestedTopic x = Topic <$> T.stripPrefix "suggested topic " x
+  unmarshallTopic x = Topic <$> T.stripPrefix "topic " x
 
 firstJust :: (a -> Maybe b) -> [a] -> Maybe b
 firstJust f xs = case mapMaybe f xs of
@@ -42,4 +58,15 @@ firstJust f xs = case mapMaybe f xs of
   _ -> Nothing
 
 pretty :: Episode -> Text
-pretty = Cast.pretty . getCast
+pretty e@(Episode cast _ _) =
+  Cast.pretty cast <> prettyTopics e
+
+prettyTopics :: Episode -> Text
+prettyTopics (Episode _ _ (Just t)) = "\nTopic: " <> t.getTopic
+prettyTopics (Episode _ topics Nothing) = prettySuggestedTopics topics
+
+prettySuggestedTopics :: [Topic] -> Text
+prettySuggestedTopics [] = ""
+prettySuggestedTopics topics =
+  "\nSuggested topics:\n"
+    <> T.unlines (Topic.getTopic <$> topics)
